4883725de7beb3432e512e8a39cb2dabf8106580
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2023-10-18 00:11:06 +0200

    V0.3
    Played around with editing of an item in a Listview. does not work however

--- ToDoList/src/main/gui/ToDoListController.java
+++ ToDoList/src/main/gui/ToDoListController.java
@@ -15,6 +15,47 @@ public class ToDoListController {
     public ListView<String> listToDo;
     public ListView<String> listInProgress;
     public ListView<String> listDone;
+    public Button btnEdit;
+    public Button btnNewToDo;
+
+    public void createNewTodoItem(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("New Todo Item");
+        dialog.setHeaderText("Add a new Todo item");
+        dialog.setContentText("Enter your todo item:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(item -> listToDo.getItems().add(item));
+    }
+
+    public void editItemInList(ActionEvent actionEvent) //cant get this method to work.
+    {
+        ListView<String> selectedList = getSelectedList();
+        if (selectedList != null) {
+            int selectedIndex = selectedList.getSelectionModel().getSelectedIndex();
+            if (selectedIndex != -1) {
+                TextInputDialog dialog = new TextInputDialog(selectedList.getItems().get(selectedIndex));
+                dialog.setTitle("Edit Todo Item");
+                dialog.setHeaderText("Edit the selected Todo item");
+                dialog.setContentText("Edit your todo item:");
+
+                Optional<String> result = dialog.showAndWait();
+                result.ifPresent(newItem -> selectedList.getItems().set(selectedIndex, newItem));
+            }
+        }
+    }
+
+    private ListView<String> getSelectedList() //helper method belonging to "editItemInList"
+    {
+        if (listToDo.isFocused()) {
+            return listToDo;
+        } else if (listInProgress.isFocused()) {
+            return listInProgress;
+        } else if (listDone.isFocused()) {
+            return listDone;
+        }
+        return null;
+    }
 
     public void moveR1(ActionEvent actionEvent) {
         String selectedItem = listToDo.getSelectionModel().getSelectedItem();
@@ -46,15 +87,5 @@ public class ToDoListController {
             listInProgress.getItems().add(selectedItem);
             listDone.getItems().remove(selectedItem);
         }
-    }
-
-    public void createNewTodoItem(ActionEvent actionEvent) {
-        TextInputDialog dialog = new TextInputDialog();
-        dialog.setTitle("New Todo Item");
-        dialog.setHeaderText("Add a new Todo item");
-        dialog.setContentText("Enter your todo item:");
-
-        Optional<String> result = dialog.showAndWait();
-        result.ifPresent(item -> listToDo.getItems().add(item));
     }
 }
